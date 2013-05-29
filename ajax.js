86ecd9f54bb8e620f469a6ed370efb4fbcd5f2e0
Rafael Almeida Erthal Hermano <rafaerthal@gmail.com>
2013-05-29 14:50:22 -0300

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2013 Rafael Almeida Erthal Hermano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
 var Ajax = new Class(function (defaults) {
     var query;
 
@@ -15,15 +31,11 @@ var Ajax = new Class(function (defaults) {
 
 	    label = label || '';
 
-        if (obj) {
+        if (obj !== null && obj !== undefined) {
     	    if (
-        		obj.constructor === Number ||
-        		obj.constructor === String ||
-        		obj.constructor === Boolean ||
-        		obj.constructor === Date
-    		) {
-                return label + '=' + escape(obj);
-    	    } else if (obj.constructor === Object) {
+    	    	obj.constructor === Object ||
+    	    	obj.constructor === Array
+	    	) {
     	        for (key in obj) {
     	            if (obj.hasOwnProperty(key)) {
     	            	query_string += query(obj[key], (label ? label + '[' + key + ']' : key)) + '&';
@@ -31,10 +43,10 @@ var Ajax = new Class(function (defaults) {
     	        }
     	        return query_string.slice(0, query_string.length - 1);
     	    } else {
-    	    	return label + '=' + escape(obj);
+    	    	return label + '=' + obj;
     	    }
         } else {
-            return '';
+            return 'null';
         }
 	};
 
